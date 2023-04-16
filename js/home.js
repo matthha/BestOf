e2cab4655f0961694ee95416682d38185368160d
matthha <122128508+matthha@users.noreply.github.com>
2023-04-15 21:04:41 -0400

    Add Original Files

---
+++ js/home.js
@@ -0,0 +1,27 @@
+tt = document.querySelector('.top')
+const showFav = () => {
+    const d = document.createElement('div')
+    const im = document.createElement('img')
+    const ds = document.createElement('div')
+
+    if (localStorage.getItem('my.fav')) {
+        a=JSON.parse(localStorage.getItem('my.fav'));
+        tt.appendChild(d);
+
+        d.className="card";
+        d.textContent=a['game']
+        d.appendChild(im);
+        im.src=a['imgl']
+        im.alt='Box art for '+a['game']
+
+        d.appendChild(ds)
+        ds.textContent=a['score']
+        ds.className='score'
+        ds.href=a['reviewlink']
+      } else {
+        tt.appendChild(d)
+        d.textContent="You have not selected a favorite game yet. Go to 'Games' to choose a favorite!"
+      }
+      console.log('Out of loop.')
+}
+showFav()
